156b5e61b450a74d9dbf4321617f27c8e26876bd
kuhljeff <kuhljeff@isu.edu>
2015-02-19 15:53:45 -0700

    dictionary

--- JeffKuhlmeier Project3/Operations.cs
+++ JeffKuhlmeier Project3/Operations.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace JeffKuhlmeier_Project3
 {
-    class Operations
+    class RPNCalc
     {
         public Queue mathQueue = new Queue();
         public Stack mathStack = new Stack();
+
+
+        public static readonly Dictionary<char, Func<int, int, int>> functions = new Dictionary<char, Func<int, int, int>>()
+        {
+            {'+',Add},
+            {'-', Subtract},
+            {'/',Divide},
+            {'*',Multiply}
+        };
+
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
 
     }
 }
