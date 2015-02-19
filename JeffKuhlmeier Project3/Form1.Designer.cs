ce85698d7736c3b5b60d69c27428cd8350993cc4
kuhljeff <kuhljeff@isu.edu>
2015-02-19 16:46:56 -0700

    .

--- JeffKuhlmeier Project3/Form1.Designer.cs
+++ JeffKuhlmeier Project3/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(136, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
@@ -67,6 +68,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
--- JeffKuhlmeier Project3/Form1.cs
+++ JeffKuhlmeier Project3/Form1.cs
@@ -29,6 +29,16 @@ namespace JeffKuhlmeier_Project3
 
         private void button1_Click(object sender, EventArgs e)
         {
+            textBox1.value.Trim();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
