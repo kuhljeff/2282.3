8a01503afd9a2f8438318b261bf9da4f74d46fa0
kuhljeff <kuhljeff@isu.edu>
2015-02-18 21:35:07 -0700

    operations

---
+++ JeffKuhlmeier Project3/Operations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeffKuhlmeier_Project3
+{
+    class Operations
+    {
+    }
+}
